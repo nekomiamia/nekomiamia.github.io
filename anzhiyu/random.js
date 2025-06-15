495d9f4dd266bef2bced1ec49104c0aafbaa6ff6
nekomiamia <407520408@qq.com>
2025-06-16 02:32:53 +0800

    Site updated: 2025-06-16 02:32:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/12/第一篇Blog/","2025/06/11/hello-world/","2025/06/12/游卡短暂的面试/"];function toRandomPost(){
+var posts=["2025/06/12/第一篇Blog/","2025/06/11/hello-world/","2025/06/12/游卡短暂的面试/","2025/06/16/小记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
