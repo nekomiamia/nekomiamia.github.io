aec8b4fc463b263fc02877c8d5fb595084cfd15e
nekomiamia <407520408@qq.com>
2025-06-21 01:59:00 +0800

    Site updated: 2025-06-21 01:58:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/12/第一篇Blog/","2025/06/11/hello-world/","2025/06/12/游卡短暂的面试/","2025/06/16/小记/","2025/06/17/Unity Profiler学习（一）/"];function toRandomPost(){
+var posts=["2025/06/12/第一篇Blog/","2025/06/11/hello-world/","2025/06/12/游卡短暂的面试/","2025/06/16/小记/","2025/06/17/Unity Profiler学习（一）/","2025/06/21/PatternPlay（1）：一切从AB包开始/","2025/06/20/微派Unity开发实习面经/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
